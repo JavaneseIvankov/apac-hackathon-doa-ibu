09d240e93c688796a73c67342a796409d8483851
arundaya <runsworkspace@gmail.com>
2025-05-14 22:52:21 +0700

    feat: added session manager, which uses react-query to deal with session

---
+++ src/features/session-manager/action.ts
@@ -0,0 +1,47 @@
+'use server';
+
+import {
+   type TSessionData,
+   defaultSession,
+   sessionOpt,
+} from '@/shared/lib/session/session';
+import { decodeToken } from '@/shared/lib/session/token';
+import { type IronSession, getIronSession } from 'iron-session';
+import { cookies } from 'next/headers';
+
+async function _getSession() {
+   const session = await getIronSession<TSessionData>(
+      await cookies(),
+      sessionOpt
+   );
+
+   if (!session.isLoggedIn) {
+      session.isLoggedIn = defaultSession.isLoggedIn;
+   }
+
+   return session;
+}
+
+export async function createSession(accessToken: string, refreshToken: string) {
+   const session = await _getSession();
+   const decoded = decodeToken(accessToken);
+
+   session.name = decoded.name;
+   session.email = decoded.email;
+   session.id = decoded.id;
+   session.isLoggedIn = true;
+   session.accessToken = accessToken;
+   session.refreshToken = refreshToken;
+
+   await session.save();
+}
+
+export async function destroySession() {
+   const session = await _getSession();
+   session.destroy();
+}
+
+export async function getSession(): Promise<IronSession<TSessionData>> {
+   const session = await _getSession();
+   return JSON.parse(JSON.stringify(session));
+}
